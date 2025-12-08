8bed634127803c636cce2c07d0a2c4baaf96c050
walana <robin.ficht@free.fr>
2025-12-08 15:04:13 +0100

    DB : imported all previously created seed/model into new architecture

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,6 +6,87 @@ const prisma = new PrismaClient();
 async function main() {
   console.log("🌱 Démarrage du seed de la base de données...");
 
+ //seeding de triages
+  const triage1 = await prisma.triage.upsert({
+    where: { name: 'Triage1' },
+    update: {},
+    create: {
+      name: 'Triage1',
+    },
+  })
+
+  const foret_bilsteinstal = await prisma.foret.upsert({
+    where: { name: 'foret_bilsteinstal' },
+    update: {},
+    create: {
+      name: 'foret_bilsteinstal',
+      triage: {
+        connect: { id: triage1.id } // <- ici on référence le triage existant
+      }
+    },
+  })
+
+   const foret_ursprung = await prisma.foret.upsert({
+    where: { name: 'foret_ursprung' },
+    update: {},
+    create: {
+      name: 'foret_ursprung',
+      triage: {
+        connect: { id: triage1.id } // <- ici on référence le triage existant
+      }
+    },
+  }) 
+
+  const parcelle1 = await prisma.parcelle.upsert({
+    where: { id: 'some-id' },
+    update: {},
+    create: {
+      name: 'parcelle1',
+      foret: {
+        connect: { id: foret_bilsteinstal.id }
+      }
+    },
+  })
+
+
+  const parcelle2 = await prisma.parcelle.upsert({
+    where: { id: 'some-id' },
+    update: {},
+    create: {
+      name: 'parcelle2',
+      foret: {
+        connect: { id: foret_bilsteinstal.id }
+      }
+    },
+  })
+
+  const parcelle3 = await prisma.parcelle.upsert({
+    where: { id: 'some-id' },
+    update: {},
+    create: {
+      name: 'parcelle3',
+      foret: {
+        connect: { id: foret_ursprung.id }
+      }
+    },
+  })
+
+  const parcelle4 = await prisma.parcelle.upsert({
+    where: { id: 'some-id' },
+    update: {},
+    create: {
+      name: 'parcelle3',
+      foret: {
+        connect: { id: foret_ursprung.id }
+      }
+    },
+  })
+
+
+  console.log({ triage1, foret_ursprung, foret_bilsteinstal})
+  console.log({ parcelle1, parcelle2, parcelle3, parcelle4})
+
+
   // Hash du mot de passe "admin" pour l'utilisateur administrateur
   const adminPasswordHash = await argon2.hash("rob");
 
